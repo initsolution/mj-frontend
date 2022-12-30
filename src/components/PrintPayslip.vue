b420dec829fcc63984eede84cfcc559384222f91
Ong Yossi Setiawan <ong.yossi.setiawan@polytron.co.id>
2022-12-30 17:17:05 +0700

    belajar pdf

---
+++ src/components/PrintPayslip.vue
@@ -0,0 +1,35 @@
+<template>
+  <v-app>
+    <v-row justify="center">
+      <v-col cols="4">
+        <v-btn @click="printPayslip" class="mt-3"> Print PaySlip </v-btn>
+      </v-col>
+    </v-row>
+  </v-app>
+</template>
+      
+  <script>
+import { mapActions, mapGetters } from "vuex";
+import { jsPDF } from "jspdf";
+export default {
+  data() {
+    return {};
+  },
+  methods: {
+    ...mapActions(["savePayslip", "actionGetPayslip"]),
+    printPayslip() {
+      const doc = new jsPDF();
+
+      doc.text("Hello world!", 10, 10);
+      doc.save("a4.pdf");
+    },
+  },
+
+  computed: {
+    ...mapGetters(["getDataAllPayslip", "getStatusPayslip"]),
+  },
+};
+</script>
+      
+      <style>
+</style>
