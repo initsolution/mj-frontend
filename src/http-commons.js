6f46eed4e748d706bcb0f1be3cb7b13a0569151f
Ong Yossi Setiawan <ong.yossi.setiawan@polytron.co.id>
2023-01-02 08:29:41 +0700

    html format untuk print payslip

--- src/http-commons.js
+++ src/http-commons.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 export default axios.create({
-    baseURL: 'http://192.168.1.5:3005/',
+    baseURL: 'http://202.157.184.138:3005/',
 
     headers: {
         "Content-Type" : "application/json"
